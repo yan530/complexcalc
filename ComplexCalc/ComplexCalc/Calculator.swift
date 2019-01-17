60560b74c17b573c0982dc3feba147b9f077ad1c
yan530 <yan530@uw.edu>
2019-01-17 11:23:35 -0800

    what's next?

--- ComplexCalc/ComplexCalc/Calculator.swift
+++ ComplexCalc/ComplexCalc/Calculator.swift
@@ -24,8 +24,24 @@ class Calculator {
         return result
     }
     
+    func add(lhs : (Int, Int), rhs : (Int, Int)) -> (Int, Int) {
+        return (lhs.0 + rhs.0, lhs.1 + rhs.1)
+    }
+    
+    func add(lhs : [String : Int], rhs : [String : Int]) -> [String : Int] {
+        return ["x": lhs["x"]! + rhs["x"]!, "y" : lhs["y"]! + rhs["y"]!]
+    }
+    
     func subtract(lhs : Int, rhs : Int) -> Int {
         return lhs - rhs
+    }
+    
+    func subtract(lhs : (Int, Int), rhs : (Int, Int)) -> (Int, Int) {
+        return (lhs.0 - rhs.0, lhs.1 - rhs.1)
+    }
+    
+    func subtract(lhs : [String : Int], rhs : [String : Int]) -> [String : Int] {
+        return ["x": lhs["x"]! - rhs["x"]!, "y" : lhs["y"]! - rhs["y"]!]
     }
     
     func multiply(lhs : Int, rhs : Int) -> Int {
